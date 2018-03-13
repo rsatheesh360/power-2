f4e0fa1c9248029d433f185306382631ee2518aa
R.Satheesh <36471895+rsatheesh360@users.noreply.github.com>
2018-03-13 17:47:53 +0530

    power

---
+++ .gitignore/power 2.py
@@ -0,0 +1,14 @@
+terms = 10
+
+# Uncomment to take number of terms from user
+#terms = int(input("How many terms? "))
+
+# use anonymous function
+result = list(map(lambda x: 2 ** x, range(terms)))
+
+# display the result
+
+print("The total terms is:",terms)
+for i in range(terms):
+   print("2 raised to power",i,"is",result[i])
+   print("2 raised to power",i,"is",result[i])
